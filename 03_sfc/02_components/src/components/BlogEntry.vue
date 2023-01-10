8767da7de64c10126ba74bd936d7606baccacca5
albert.greinoecker@gmail.com <albert.greinoecker@gmail.com>
2023-01-10 13:57:44 +0100

    added Components

---
+++ 03_sfc/02_components/src/components/BlogEntry.vue
@@ -0,0 +1,36 @@
+
+
+<template>
+  <div class="greetings">
+      <table>
+          <tr>
+            <td>  <button @click="$emit('blogclick',entry.id)">{{entry.id}}</button></td>
+            <td>{{entry.text }}</td>
+      </tr>
+      </table>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+
+<script>
+export default {
+  props : {
+    entry:
+        {
+          type : Object,
+          default: {id: 1, text: 'default'}
+        }
+  },
+  emits: ['blogclick'],
+
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
